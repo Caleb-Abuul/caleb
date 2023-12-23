89fc592147e797c2cc302b9abf7207e4699eb0cb
Caleb-Abuul <abuulcaleb@gmail.com>
2023-12-23 15:29:10 +0100

    made a few changes to the aside menu

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let openMenu = document.getElementById('menu');
 let closeMenu = document.getElementById('close');
 let asideNav = document.getElementById('aside');
+let projects = document.getElementById('my-projects');
 
 openMenu.addEventListener('click', ()=>{
     asideNav.style.right = '0';
 })
 
 closeMenu.addEventListener('click', ()=>{
+    asideNav.style.right = '-200px';
+})
+
+projects.addEventListener('click', ()=>{
     asideNav.style.right = '-200px';
 })
